e915fdc9a6ea6c84ca69d22c072668ff1b952212
tk0515 <thaliagkennedy@gmail.com>
2024-06-13 17:28:12 -0400

    Some more guest functionality updated

--- application/guest.py
+++ application/guest.py
@@ -11,4 +11,13 @@ def guestMain():
         elif confirm != "n":
             print("I will take that as a no...")
 
-    print("Great!")
+    print("Great! Now here is a list of available toppings: ")
+    print("Cheese, pepperoni, sausage, onion, green pepper, mushroom, black olive, tomato, basil, ham, pineapple, bacon")
+    print("What pizza toppings does the group like?")
+    print("Please enter toppings one at a time, hitting ENTER in between toppings. When done, type 'done'.")
+    inputOver = False
+    while(inputOver == False):
+        topping = input()
+        print(topping)
+        if topping == "done":
+            inputOver = True
