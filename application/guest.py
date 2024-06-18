601d0a7f3ea6180e13ed77ba5c3586f82340978f
tk0515 <thaliagkennedy@gmail.com>
2024-06-18 14:32:03 -0400

    Updated user input abilities

--- application/guest.py
+++ application/guest.py
@@ -1,6 +1,8 @@
 # Guest functions here
 
 def guestMain():
+    pizzaToppings = ["cheese", "pepperoni", "sausage", "onion", "green pepper", "black olive", "tomato", "basil", "ham", "pineapple",
+                "bacon"] 
     print("Welcome to guest mode!")
     eaters = True
     while(eaters):
@@ -16,8 +18,18 @@ def guestMain():
     print("What pizza toppings does the group like?")
     print("Please enter toppings one at a time, hitting ENTER in between toppings. When done, type 'done'.")
     inputOver = False
+    userToppings = []
     while(inputOver == False):
-        topping = input()
-        print(topping)
+        proper = False
+        topping = input("> ")
+        for i in pizzaToppings:
+            if topping == i:
+                proper = True
+                userToppings.append(topping)
         if topping == "done":
             inputOver = True
+        elif proper == False:
+            print("That is not one of the listed pizza toppings.")
+    print("You have selected the following toppings")
+    for y in userToppings:
+        print(y)
