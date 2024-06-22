cfa5fa4d097d13045b243984ae809c3a7c0ca721
tk0515 <thaliagkennedy@gmail.com>
2024-06-22 15:53:15 -0400

    Updated calculations so it calculates the amount of pizzas that should be ordered in guest mode. Does not currently suggest what pizzas should have what toppings

--- application/guest.py
+++ application/guest.py
@@ -2,19 +2,21 @@
 
 def guestMain():
     pizzaToppings = ["cheese", "pepperoni", "sausage", "onion", "green pepper", "black olive", "tomato", "basil", "ham", "pineapple",
-                "bacon"] 
+                "bacon", "mushroom"] 
     print("Welcome to guest mode!")
     eaters = True
     while(eaters):
-        number = input("How many people in your group want pizza? ")
-        confirm = input("To confirm, " + number + " people want pizza? [y/n] ")
+        number = int(input("How many people in your group want pizza? "))
+        confirm = input("To confirm, " + str(number) + " people want pizza? [y/n] ")
         if confirm == "y" or confirm == "yes" or confirm == "Yes":
             eaters = False
         elif confirm != "n":
             print("I will take that as a no...")
 
+    print()
     print("Great! Now here is a list of available toppings: ")
     print("Cheese, pepperoni, sausage, onion, green pepper, mushroom, black olive, tomato, basil, ham, pineapple, bacon")
+    print()
     print("What pizza toppings does the group like?")
     print("Please enter toppings one at a time, hitting ENTER in between toppings. When done, type 'done'.")
     inputOver = False
@@ -27,9 +29,45 @@ def guestMain():
                 proper = True
                 userToppings.append(topping)
         if topping == "done":
-            inputOver = True
+            if len(userToppings) == 0:
+                print("You must choose at least one topping!")
+            else:
+                inputOver = True
         elif proper == False:
             print("That is not one of the listed pizza toppings.")
-    print("You have selected the following toppings")
-    for y in userToppings:
-        print(y)
+    print("You have selected the following toppings:")
+    print(*userToppings, sep = ", ")
+    print()
+    print("Calculating...")
+
+    if number < 8:
+        calculation(0, number)
+    elif (number % 8) == 0:
+        calculation(number, 0)
+    else:
+        remainder = number%8
+        calculation(number-remainder, remainder)
+
+def calculation(multiple, remainder):
+    easyLarge = multiple / 8
+    largeZa = int(easyLarge*3)
+
+    # remaining slices from left over people
+    slices = remainder*3
+    smallZa = 0
+
+    if (slices%8) == 0:
+        largeZa += slices/8
+    else:
+        newRemainder = slices%8
+        largeZa += int((slices-newRemainder)/8)
+        if newRemainder < 5:
+            smallZa += 1
+        else:
+            largeZa += 1
+
+    print("You should order: ")
+    if smallZa == 0:
+        print(str(largeZa) + " Large Pizzas")
+    else:
+        print(str(largeZa) + " Large Pizzas and " + str(smallZa) + " Small Pizza")
